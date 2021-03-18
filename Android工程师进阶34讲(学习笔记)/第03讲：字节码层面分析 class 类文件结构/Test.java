a3c2260bba4c12aab8bfcdd48468d01bd4f72ecc
LAPTOP-4S4U6G5J\lenovo <fishwjy@163.com>
2021-03-18 16:08:09 +0800

    增加class文件结构示例

---
+++ Android工程师进阶34讲(学习笔记)/第03讲：字节码层面分析 class 类文件结构/Test.java
@@ -0,0 +1,10 @@
+import java.io.Serializable; 
+
+public class Test implements Serializable, Cloneable{ 
+	  private int num = 1; 
+	  String str = "abc"; 
+	  
+	  public int add(int i) { 
+	      num = num + i; return num; 
+	}
+}
